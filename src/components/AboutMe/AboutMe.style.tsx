f00a040ce35e5671a3314eda25d15c007649e68b
Daniel Gonçalves <daniel.tekla123@gmail.com>
2024-08-23 14:44:03 -0300

    initial configuration React-Vite

---
+++ src/components/AboutMe/AboutMe.style.tsx
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+export const AboutMeContainer = styled.section`
+    width: 100%;
+    height: auto;
+    background-color: #DCDCDC;
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    gap: 10px;
+    padding: 80px 10px;
+
+    @media (max-width: 940px) {
+        grid-template-columns: 1fr;
+        padding: 80px 10px;
+    }
+`
+// Seção da Foto 
+export const AboutMePhotoSection = styled.div`
+    display: grid;
+    place-content: center;
+
+    @media (max-width: 940px) {
+        display: none;
+    }
+`
+
+export const AboutMePhoto = styled.img`
+    width: 18rem;
+    border-radius: 50%;
+`
+
+// Seção de título, texto e botão
+export const AboutMeDetailSection = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    line-height: 1.5;
+    padding: 0 20px;
+    gap: 30px;
+`
+
+export const AboutMeTitle = styled.h1`
+    font-size: clamp(1.8em, 5vw, 2.5em);
+`
+
+export const AboutMeText = styled.p`
+    font-size: clamp(0.7em, 2vw, 1rem);
+`
+
+export const ButtonDiv = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const ButtonMore = styled.button`
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    background-color: rgba(0,0,0, 0);
+    padding: 10px;
+    border: 1px solid #000000;
+    border-radius: 8px;
+    font-size: clamp(0.8rem, 3vw, 1.2rem);
+    transition: 0.5s;
+
+    &:hover {
+        background-color: black;
+        color: white;
+        transition: 0.5s;
+    }
+`
+
+export const AncorDownload = styled.a`
+    all: unset;
+`
---
+++ src/components/AboutMe/AboutMe.tsx
@@ -0,0 +1,48 @@
+import {
+    AboutMeContainer,
+    AboutMePhoto,
+    AboutMeDetailSection,
+    AboutMeTitle,
+    AboutMeText,
+    AboutMePhotoSection,
+    ButtonMore,
+    ButtonDiv,
+    AncorDownload
+} from "./AboutMe.style";
+
+const AboutMe = () => {
+
+    return (
+        <AboutMeContainer id="Resume">
+
+            {/* Foto - Sobre Mim */}
+            <AboutMePhotoSection>
+                <AboutMePhoto src="./src/assets/images/perfil-sobremim.png" alt="Daniel Gonçalves" />
+            </AboutMePhotoSection>
+
+            {/* Text - Sobre Mim */}
+            <AboutMeDetailSection>
+                <AboutMeTitle>
+                    Sobre Mim
+                </AboutMeTitle>
+                <AboutMeText>
+                    Meu nome é Daniel Gonçalves dos Santos. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas
+                    na Descomplica Faculdade Digital, com previsão de conclusão em 2025. Tenho formação em Administração de
+                    Empresas e uma sólida experiência em operações aeroportuárias. Sou apaixonado por tecnologia e estou em
+                    transição para a área de desenvolvimento de software, buscando aplicar minhas habilidades em projetos que
+                    unam eficiência e inovação. Nos meus momentos livres, gosto de explorar novas tecnologias e aprimorar meus
+                    conhecimentos em programação.
+                </AboutMeText>
+                <ButtonDiv>
+                    <ButtonMore>
+                        <AncorDownload href="/src/assets/docs/CV Daniel Goncalves.pdf" download>
+                            Resumo
+                        </AncorDownload>
+                    </ButtonMore>
+                </ButtonDiv>
+            </AboutMeDetailSection>
+        </AboutMeContainer>
+    )
+}
+
+export default AboutMe;
